c05daa26a3b077c506ec4f4b37ed25aec5162a0c
David Mahéo <david.maheo@orange.com>
2024-12-01 20:50:44 +0100

    generate files

---
+++ src/2021/d6.py
@@ -0,0 +1,19 @@
+""""""
+from ..day import DayBase
+
+example = """"""
+
+
+class Day(DayBase):
+    def do(self):
+        self.test(None, None, example)
+        self.run(None, None)
+
+    @staticmethod
+    def part1(data=example):
+        pass
+
+    @staticmethod
+    def part2(data=example):
+        pass
+        
